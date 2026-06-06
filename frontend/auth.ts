5588ba15b37b711cab4d429be6a16bafca9a21c6
juice0119 <3220ju@naver.com>
2026-06-06 18:56:49 +0900

    feat: add auth.js credential 설정

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,8 +1,55 @@
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "./prisma";
+import { prisma } from "@/prisma";
+import { comparePassword } from "./lib/password-utils";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  useSecureCookies: process.env.NODE_ENV === "production",
+  trustHost: true,
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  secret: process.env.AUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "이메일",
+          type: "email",
+          placeholder: "이메일 입력",
+        },
+        password: {
+          label: "비밀번호",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials || !credentials.email || !credentials.password) {
+          throw new Error("이메일과 비밀번호를 입력해주세요.");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email as string,
+          },
+        });
+
+        if (!user) throw new Error("존재하지 않는 이메일 입니다.");
+
+        const matchedPassword = comparePassword(
+          credentials.password as string,
+          user.hashedPassword as string,
+        );
+
+        if (!matchedPassword) throw new Error("비밀 번호가 일치하지 않습니다.");
+
+        return user;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {},
+  callbacks: {},
 });
