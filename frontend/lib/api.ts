887a7f792030c99d8c5c1ad1106643f3a5ab42f6
juice0119 <3220ju@naver.com>
2026-06-14 13:44:42 +0900

    feat(frontend): 강의 생성 - 강의 제목 입력 레이아웃 추가

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,28 @@
 'use server';
 
-import { categoriesControllerFindAll, coursesControllerFindAll } from '@/generated/openapi.ts';
+import {
+  categoriesControllerFindAll,
+  coursesControllerCreate,
+  coursesControllerFindAll,
+} from '@/generated/openapi.ts';
+
+function serializeApiResponse<T>({ data, error }: { data: T | undefined; error: unknown }): {
+  data: T | null;
+  error: { message: string | null; isError: boolean } | null;
+} {
+  if (error === undefined && data !== undefined) return { data, error: null };
+
+  if (
+    typeof error === 'object' &&
+    error !== null &&
+    'message' in error &&
+    typeof error.message === 'string'
+  ) {
+    return { data: null, error: { message: error.message, isError: true } };
+  }
+
+  return { data: null, error: { message: '서버에 요청중 오류가 발생했습니다.', isError: true } };
+}
 
 export const getAllCategories = async () => {
   const { data, error } = await categoriesControllerFindAll();
@@ -11,3 +33,21 @@ export const getAllInstructorCourses = async () => {
   const { data, error } = await coursesControllerFindAll();
   return { data, error };
 };
+
+export async function createCourse(title: string) {
+  const response = await coursesControllerCreate({
+    body: {
+      title,
+      //@Todo slug 변환 추가 필ㅛ
+      slug: title,
+      shortDescription: '',
+      description: '',
+      thumbnailUrl: '',
+      price: 0,
+      discountPrice: 0,
+      level: 'BEGINNER',
+      categoryIds: [],
+    },
+  });
+  return serializeApiResponse(response);
+}
